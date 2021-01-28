d2375050ad0f59b165f6432e007794ea94f0dc21
o.yrtsvr@gmail.com <o.yrtsvr@gmail.com>
2021-01-29 00:30:23 +0100

    show answers function changed in js

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 document.addEventListener("click", (e) => {
   let classlist = e.target.classList;
+  let answers = Array.from(document.getElementsByClassName("a"));
+
+  console.log(answers);
   if (classlist == "q") {
+    answers.forEach((answer) => {
+      console.log(answer);
+      answer.classList.remove("selected");
+    });
     e.target.nextElementSibling.classList.add("selected");
   }
 });
